59014c7b7963b246ac20aa6a8fcc2cf63fdf66ad
Dave Yates <daveyates@Daves-MacBook-Pro.local>
2020-06-02 16:37:11 +0100

    Setting up unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,78 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('it groups postcodes without duplicates', () => {
+
+  // Given a dataset
+
+  // We get an array of postcodes grouped, without duplicates
+
+});
+
+test('it gets the outward code of a postcode', () => {
+
+  // Given a postcode 
+
+  // We get just the outward portion of the postcode
+
+});
+
+test('it handles postcodes without spaces', () => {
+
+  // Given a postcode without spaces
+
+  // We are returned the correct outward code e.g M4A23 would return M4, M414FG would return M41
+
+});
+
+
+test('it handles incorrect postcodes', () => {
+
+  // Given a postcode that is incorrectly formatted e.g using numbers, or the wrong number of characters
+
+  // We are returned an error / a null object
+
+});
+
+
+test('it includes total count of postcodes', () => {
+
+  // Given a list of postcodes
+
+  // We are returned an array of postcode objects, with the total count of postcodes as a root property
+
+});
+
+
+test('it produces a list of postcodes with the  percentage of total amount', () => {
+
+  // Given an array of postcodes with counts
+
+  // We are returned an array of postcode objects, with the percentage of total postcode
+
+});
+
+test('it produces a list of top 5 postcodes', () => {
+
+  // Given a an array of postcodes with counts 
+
+  // We get the top 5 postcodes
+
+});
+
+test('it includes the datestamp if one is provided', () => {
+
+  // Given a datestamp in the object
+
+  // We are returned the same datestamp
+
+});
+
+test('it includes todays date as a datestamp if one is not provided', () => {
+
+  // Given no datestamp in the object
+
+  // We are returned todays datestamp
+
+});
