9ee30890d15d49b55bc2a2003753e5dc98199241
Dave Yates <daveyates@Daves-MacBook-Pro.local>
2020-06-03 14:44:00 +0100

    Updates

---
+++ precache-manifest.433490476de1b89923223f2cc424073c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2a583738a9cd84c678e25d39d87387",
+    "url": "/open-pharmacy/index.html"
+  },
+  {
+    "revision": "3515e6ab3ec910b779b5",
+    "url": "/open-pharmacy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "802eb4cb3faca88073f6",
+    "url": "/open-pharmacy/static/js/2.bb05609f.chunk.js"
+  },
+  {
+    "revision": "a87f6040463da92f34f4b8f7708689af",
+    "url": "/open-pharmacy/static/js/2.bb05609f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3515e6ab3ec910b779b5",
+    "url": "/open-pharmacy/static/js/main.810697d9.chunk.js"
+  },
+  {
+    "revision": "4c91df887806e63f9b0c",
+    "url": "/open-pharmacy/static/js/runtime-main.54cb6481.js"
+  }
+]);
